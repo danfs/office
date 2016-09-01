ff25cf0b47bcc148d54542b25bd17d97ac590943
dan <dan@yossarianlives.com>
2016-09-01 21:22:58 +0100

    correct boot files

--- dev/boot.ts
+++ dev/boot.ts
@@ -6,8 +6,8 @@ import {HTTP_PROVIDERS} from 'angular2/http';
 
 import {provide} from 'angular2/core';
 import {
-  HashLocationStrategy
-  LocationStrategy,
+  HashLocationStrategy,
+  LocationStrategy
 } from 'angular2/router';
 
 bootstrap(AppComponent, [HTTP_PROVIDERS,ROUTER_PROVIDERS,provide(LocationStrategy, {useClass: HashLocationStrategy})]);
